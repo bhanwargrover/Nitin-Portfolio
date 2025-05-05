1b3662be0046cd3c94e424680445891aae573cd1
Bhanwar Grover <bhavargrover4@gmail.com>
2025-05-05 17:36:31 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -18,7 +18,7 @@ const Contact: React.FC = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      const response = await fetch('https://nitin-portfolio-hjh3.onrender.com/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
